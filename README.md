199ccf4cf0126cfa99439c07479cba84dc46e320
Andreas Ejdesgaard <andreasejdesgaard@protonmail.com>
2020-07-11 16:40:48 +0200

    Add README.md, License and contact information

---
+++ README.md
@@ -0,0 +1,22 @@
+# Time_change_gsc
+
+This program can be used to generate a time reset password for Pokémon Gold, Silver or Crystal.
+
+Compiling this program requires the Rust compiler, and Cargo.
+It should work on any system supported by Rust.
+
+### Running
+
+```
+cargo run --release
+```
+
+### Installing
+Generally not neccessary for this program, but still possible
+```
+cargo install --path .
+```
+
+## License
+
+Time_change_gsc is licensed under the GPLv3 [license](LICENSE) or later.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+/*
+	This file is part of time_change_gsc.
+
+	time_change_gsc is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	time_change_gsc is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with time_change_gsc.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use std::io;
 
 fn poke_char_value(letter:char) -> u16 {
